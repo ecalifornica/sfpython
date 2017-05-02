bde587b961873343641205a535e7424077c7fe3b
Simeon Franklin <sfranklin@twitter.com>
2017-05-02 12:52:17 -0700

    Add email config to settings

--- sfpython/sfpython/prod_settings.py
+++ sfpython/sfpython/prod_settings.py
@@ -8,4 +8,11 @@ STATIC_URL = '/resources/'
 DEBUG = False
 ALLOWED_HOSTS = ['.bayareapython.com']
 
-SECRET_KEY = os.getenv('BAYAREAPYTHON_SECRET'),
+SECRET_KEY = os.getenv('BAYAREAPYTHON_SECRET')
+
+DEFAULT_FROM_EMAIL = 'sf@simeonfranklin.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'sf@simeonfranklin.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.getenv('EMAILPW')
